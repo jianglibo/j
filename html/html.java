6ed05c5fab78a6900435382288a9765e9c8a0848
jianglibo <jianglibo@gmail.com>
2023-12-08 16:32:31 +0800

    Refactor html class to remove unused code and commented out sections

--- html/html.java
+++ html/html.java
@@ -4,14 +4,13 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.concurrent.Callable;
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Parameters;
 
 @Command(name = "html", mixinStandardHelpOptions = true, version = "html 0.1", description = "html made with jbang")
-class html implements Callable<Integer> {
+class html {
 
     static final String blankHtml = """
             <!doctype html>
@@ -34,19 +33,8 @@ class html implements Callable<Integer> {
         System.exit(exitCode);
     }
 
-    @Override
-    public Integer call() throws Exception { // your business logic goes here...
-        java.nio.file.Files.list(java.nio.file.Path.of("")).forEach(System.out::println);
-        System.out.println("Hello " + greeting);
-        return 0;
-    }
-
     @Command(mixinStandardHelpOptions = true)
     void blankHtml(@Parameters(description = "name of the new html.") String name) throws IOException {
-        // if (name == null || name.isBlank()) {
-        //     System.err.println("name is required");
-        //     return;
-        // }
         Files.writeString(Path.of(name), blankHtml);
     }
 }
